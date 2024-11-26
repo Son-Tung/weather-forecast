8bbb76257d24e029499953bdd3b6c039eff189bd
Tung. Nguyen Son - CMC Global DU1.20 <nstung4@cmcglobal.vn>
2024-11-26 10:40:16 +0700

    them snow weather image

--- src/assets/images/weatherImages.tsx
+++ src/assets/images/weatherImages.tsx
@@ -2,11 +2,13 @@ import RainyImage from '../images/Rain.png'
 import CloudyImage from '../images/Cloudy.png'
 import SunnyImage from '../images/Sunny.png'
 import MistImage from '../images/Mist.png'
+import SnowImage from '../images/Snowdy.png'
 
 export const weatherImages: { [key: string]: string } = {
   clear: SunnyImage,
   rain: RainyImage,
   clouds: CloudyImage,
   mist: MistImage,
-  haze: MistImage
+  haze: MistImage,
+  snow: SnowImage
 }
